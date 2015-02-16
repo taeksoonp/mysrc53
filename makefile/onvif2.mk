824ea152206ecb3ca7187acde947814e20bcc4f9
taeksoonp <taeksoonp@gmail.com>
2015-02-16 12:54:29 +0900

    minor fix

--- makefile/onvif2.mk
+++ makefile/onvif2.mk
@@ -17,7 +17,7 @@ TARGET = nvt
 Bm_objs = word_analyse.o util.o sys_os.o sys_log.o sys_buf.o ppstack.o base64.o sha1.o
 Onvif_objs = xml_node.o soap.o onvif_probe.o onvif_pkt.o onvif.o hxml.o http_rx.o \
 	soap_parser.o onvif_device.o http_parse.o http_cln.o onvif_timer.o onvif_event.o \
-	onvif_api.o onvif_ptz.o onvif_util.o onvif_media.o onvif_image.o 
+	onvif_api.o onvif_ptz.o onvif_util.o onvif_media.o onvif_image.o onvif_cm.o
 Wgi_objs = ipc.o conf.o
 OBJS = $(addprefix bm/,$(Bm_objs)) $(addprefix onvif/,$(Onvif_objs)) \
 	$(addprefix wgi/,$(Wgi_objs)) $(TARGET).o
