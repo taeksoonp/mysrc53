9a2640430b872673c7110bab76ac349cc0aa872a
taeksoonp <taeksoonp@gmail.com>
2015-08-28 09:01:33 +0900

    hi_util.mk 추가

--- python/xmltt.py
+++ python/xmltt.py
@@ -6,28 +6,26 @@
 import sys, getopt
 from xml.etree.ElementTree import dump, parse
 
-#options
-be_quiet = False
+# options
+option_silence = False
 
 def notice(*objs):
     print(*objs, file=sys.stderr)
-    
+
 def report(*objs):
-    global be_quiet
-    
-    if (be_quiet):
-        return
-    
-    print(*objs)
+    global option_silence
+
+    if (not option_silence):
+        print(*objs)
 
 def parse_xml(file, tag1):
     tree = parse(file)
     root = tree.getroot()
     ns = root.tag.split('}')[0] + ('}')
-    
+
 #    notice("root tag:", root.tag)
 #    notice("xmlns:", ns)
-    
+
 # Searching all subelements(fast enough).
     element = root.find('.//' + ns + tag1)
     if element == None:
@@ -37,7 +35,7 @@ def parse_xml(file, tag1):
     rows = []
     for child in element:
         rows.append(child.text)
-    
+
     if (len(rows) == 0):
         report(rows)
         return
@@ -47,8 +45,8 @@ def parse_xml(file, tag1):
         if i != val:
             report(rows)
             return
-        
-#The node is good.
+
+# The node is good.
     report(val)
 
 def usage():
@@ -57,40 +55,40 @@ Testing xml node. v1.0
 notice("usage: testxml -t tag <filename>
 -h, --help:\tThis message
 -t, --tag:\tTag name is mandatory.
--q, --quient:\tDon't print out result.
+-s, --silence:\tDon't print out result.
 ''')
-    
+
 def main(argv):
-    global be_quiet
+    global option_silence
     tag = ''
-    
+
     try:
-        opts, args = getopt.getopt(argv, 'hqt:', ['help', 'quiet', 'tag='])
+        opts, args = getopt.getopt(argv, 'hst:', ['help', 'silence', 'tag='])
 
     except getopt.GetoptError:
         usage()
         notice("I've gotten error.")
         exit(2)
-        
+
     for opt, arg in opts:
         if opt == '-h' or opt == '--help':
             usage()
-            exit()
-            
+            exit(2)
+
         elif opt in ("-t", "--tag"):
             tag = arg
 
-        elif opt in ("-q", "--quiet"):
-            be_quiet = True
-            
+        elif opt in ("-s", "--silence"):
+            option_silence = True
+
     if (tag == ''):
         usage()
-        exit(1)
-        
+        exit(2)
+
     file = args[0]
-    
+
     notice('args/tag: %s/%s' % (args, tag))
-    
+
     parse_xml(file, tag)
 
 if __name__ == "__main__":
