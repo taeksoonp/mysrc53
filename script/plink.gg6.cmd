ac629e1aaa2187fb563d48ea0f349f6bcbfb0741
taeksoonp <taeksoonp@gmail.com>
2019-01-17 09:57:22 +0900

    eclipse용 원격 명령 통합

--- script/plink.gg6.cmd
+++
@@ -1,5 +0,0 @@
-rem https://stackoverflow.com/questions/2772456/string-replacement-in-batch-file
-
-set arg1=%1
-set Where=%arg1://192168.217.165=/home%
-plink  ts.p@192.168.217.165 "cd %Where% && %2 %3 %4 %5 %6 %7 %8 %9"
--- script/plink.ptslinux.cmd
+++ script/plink.ptslinux.cmd
@@ -1,5 +1,18 @@
 rem https://stackoverflow.com/questions/2772456/string-replacement-in-batch-file
 
 set arg1=%1
-set Where=%arg1://PTSLINUX.MSHOME.NET=/home%
-plink  tsp@ptslinux.mshome.net "cd %Where% && %2 %3 %4 %5 %6 %7 %8 %9"
+
+rem //PTSLINUX.MSHOME.NET/tsp/
+if "%arg1:~2,19%" == "PTSLINUX.MSHOME.NET" (
+	set Where=%arg1://PTSLINUX.MSHOME.NET/tsp/=%
+	set Ip=ptslinux.mshome.net
+
+) else if "%arg1:~2,15%" == "192.168.217.165" ( 
+	set Where=%arg1://192.168.217.165/tsp/=%
+	set Ip=192.168.217.165
+
+) else (
+	echo ¾îµð?
+)
+
+plink tsp@%Ip% "export LANG=C;cd %Where% && %2 %3 %4 %5 %6 %7 %8 %9"
